cf762b992b63d3a4b2eba404a1a1f99af5f79e1f
Veronija <veronija@bintegra.com>
2024-12-10 22:26:37 +0100

    Fix

--- backend/routes/groupRouter.js
+++ backend/routes/groupRouter.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const Group = require('../schemas/group');
-const GroupService = require('../services/groupService');
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = "Vkm123vkm$$$";
+const GroupService = require('../services/GroupService');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'Vkm123vkm$$$';
 
 const router = express.Router();
 
 const authenticateToken = (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Access token required" });
+  const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Access token required' });
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "Invalid token" });
+    if (err) return res.status(403).json({ message: 'Invalid token' });
     req.user = user;
-    next();      
+    next();
   });
 };
 
 // Get all groups
-router.get('/', authenticateToken,  async (req, res) => {
+router.get('/', authenticateToken, async (req, res) => {
   try {
     const groups = await GroupService.getGroups();
     res.json(groups);
@@ -30,7 +30,7 @@ router.get('/', authenticateToken,  async (req, res) => {
 // Get a single group by ID
 router.get('/:id', authenticateToken, async (req, res) => {
   try {
-    const group = (new GroupService(req.params.id)).getGroup();
+    const group = new GroupService(req.params.id).getGroup();
     if (!group) return res.status(404).json({ message: 'Group not found' });
     res.json(group);
   } catch (err) {
@@ -51,7 +51,7 @@ router.post('/', authenticateToken, async (req, res) => {
 // Update group by ID
 router.put('/:id', authenticateToken, async (req, res) => {
   try {
-    const updatedGroup = (new GroupService(req.params.id)).updateGroup(req.body);
+    const updatedGroup = new GroupService(req.params.id).updateGroup(req.body);
     if (!updatedGroup)
       return res.status(404).json({ message: 'Group not found' });
     res.json(updatedGroup);
@@ -64,7 +64,7 @@ router.put('/:id', authenticateToken, async (req, res) => {
 router.delete('/:id', authenticateToken, async (req, res) => {
   try {
     const deletedGroup = new GroupService(req.params.id);
-    await deletedGroup.deleteGroup()
+    await deletedGroup.deleteGroup();
     if (!deletedGroup)
       return res.status(404).json({ message: 'Group not found' });
     res.json({ message: 'Group deleted' });
