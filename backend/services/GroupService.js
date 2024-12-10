16b404de92919e218e52cbc56573daaef7002a25
Veronija <veronija@bintegra.com>
2024-12-10 21:43:33 +0100

    test

--- backend/services/GroupService.js
+++ backend/services/GroupService.js
@@ -1,31 +1,33 @@
-const Group = require("../schemas/group");
+const Group = require('../schemas/group');
 
 class GroupService {
-    _id;
+  _id;
 
-    constructor(id) {
-        this._id = id;
-    }
+  constructor(id) {
+    this._id = id;
+  }
 
-    static async getGroups() {
-        return await Group.find().populate('members').exec()
-    }
+  static async getGroups() {
+    return await Group.find().populate('members').exec();
+  }
 
-    async getGroup() {
-        return await Group.findById(this._id).populate('members').exec()
-    }
+  async getGroup() {
+    return await Group.findById(this._id).populate('members').exec();
+  }
 
-    static async createGroup(group) {
-        return await new Group(group).save()
-    }
+  static async createGroup(group) {
+    return await new Group(group).save();
+  }
 
-    async updateGroup(properties) {
-        return await Group.findByIdAndUpdate(this._id, properties, { new: true }).exec()
-    }
+  async updateGroup(properties) {
+    return await Group.findByIdAndUpdate(this._id, properties, {
+      new: true,
+    }).exec();
+  }
 
-    async deleteGroup() {
-        return await Group.findByIdAndDelete(this._id).exec()
-    }
+  async deleteGroup() {
+    return await Group.findByIdAndDelete(this._id).exec();
+  }
 }
 
-module.exports = GroupService
+module.exports = GroupService;
